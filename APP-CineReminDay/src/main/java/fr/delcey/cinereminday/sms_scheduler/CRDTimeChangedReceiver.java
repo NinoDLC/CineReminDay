868cbd795180a58e5d28508caa2756152f032282
Nino <anthony@delcey.fr>
2017-07-04 20:03:32 +0200

    [F] Listen to time / date / zone changes and reschedule alarm

---
+++ APP-CineReminDay/src/main/java/fr/delcey/cinereminday/sms_scheduler/CRDTimeChangedReceiver.java
@@ -0,0 +1,18 @@
+package fr.delcey.cinereminday.sms_scheduler;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import fr.delcey.cinereminday.local_code_manager.CRDTimeManager;
+
+/**
+ * Created by Nino on 04/07/2017.
+ */
+
+public class CRDTimeChangedReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        CRDTimeManager.scheduleWeeklyAlarm(context);
+    }
+}
